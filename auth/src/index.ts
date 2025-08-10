f08b1e906f198358c54f1b496402dd9e9945c601
Asem Dreibati <asemdreibati97@gmail.com>
2025-08-10 09:00:28 +0300

    add comment to index.ts in auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -25,7 +25,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!!!!!!!!");
+    console.log("Listening on port 3000!!!!!!!!!!");
   });
 };
 
